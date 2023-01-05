ac21dfe73b9cf0bdb08aaedd4553054c40315ac2
tri273543149 <tri273543149cmnd@gmail.com>
2023-01-05 21:23:05 +0700

    commit 1

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options) { }
+
+        DbSet<Region> Regions { get; set; }
+        DbSet<Walk> Walks { get; set; }
+        DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
